055b37b6dd4d6b8b1053e8c7f9914efeb2b756dd
kotaindah55 <kotaindah55@gmail.com>
2026-07-25 02:54:25 +0300

    fix: use `console.debug()` instead of `console.log()`

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
-import { type App, type Editor, type EventRef, MarkdownView, Plugin } from './obsidian';
-import { patchCursorLayer } from './patch';
+import {
+	type App,
+	type Debouncer,
+	type Editor,
+	type EventRef,
+	type PluginManifest,
+	debounce,
+	Plugin
+} from './obsidian';
+import { adjustCursorLayer, patchCursorLayerConfig } from './patch';
 import { AnimatedCursorSettingTab } from './setting-tab';
 import { tableCellObserver } from './observer';
-import { hookCursorPlugin } from './hook';
-import type { CursorPluginInstance } from './types';
+import { hookCursorPluginInstance } from './hook';
+import type { CursorPlugin } from './types';
+import { cursorLayerCleanUp } from './clean-up';
 
 export interface AnimatedCursorSettings {
 	useTransform: boolean;
@@ -15,18 +24,27 @@ function getDefaultSettings(): AnimatedCursorSettings {
 
 export class AnimatedCursorPlugin extends Plugin {
 	public override readonly settings: AnimatedCursorSettings;
+	public readonly requestSave: Debouncer<[], void>;
+	public cursorPlugin: CursorPlugin | null;
 
 	/**
 	 * If any, it indicates that the cursor plugin is already patched.
 	 */
-	private alreadyPatched!: boolean;
-	private tryPatchRef?: EventRef;
-	private cursorPlugin?: CursorPluginInstance;
+	private alreadyPatched: boolean;
+	private patchAttemptRef?: EventRef;
+
+	public constructor(app: App, manifest: PluginManifest) {
+		super(app, manifest);
+
+		this.settings = getDefaultSettings();
+		this.requestSave = debounce(this.saveSettings.bind(this), 80);
+		this.alreadyPatched = false;
+		this.cursorPlugin = null;
+	}
 
 	public override async onload(): Promise<void> {
 		await this.loadSettings();
 
-		this.alreadyPatched = false;
 		this.addSettingTab(new AnimatedCursorSettingTab(this.app, this));
 		this.registerEditorExtension([
 			tableCellObserver,
@@ -35,26 +53,18 @@ export class AnimatedCursorPlugin extends Plugin {
 
 		let activeEditor = this.app.workspace.activeEditor?.editor;
 		if (activeEditor) this.tryPatch(activeEditor);
-		else this.tryPatchRef = this.app.workspace.on(
-			'editor-selection-change',
-			this.tryPatch.bind(this)
-		);
+		else this.patchAttemptRef = this.app.workspace.on('editor-selection-change', this.tryPatch.bind(this));
 
 		this.app.workspace.trigger('parse-style-settings');
 
-		console.log('Load Animated Cursor plugin');
-	}
-
-	public async loadSettings(): Promise<void> {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	public async saveSettings(): Promise<void> {
-		await this.saveData(this.settings);
+		console.debug('Load Animated Cursor plugin');
 	}
 
 	public override onunload(): void {
 		this.cancelPatchAttempt();
+		console.debug('Unload Animated Cursor plugin');
+	}
+
 	private async loadSettings(): Promise<void> {
 		Object.assign(this.settings, await this.loadData());
 	}
@@ -64,47 +74,44 @@ export class AnimatedCursorPlugin extends Plugin {
 	}
 
 	/**
-	 * Try to patch the cursor plugin on corresponding editor. Should only be
+	 * Patch the cursor plugin on corresponding editor. Should only be
 	 * run at the first time, or when the previous attemps failed.
-	 * 
-	 * Used as `editor-selection-change` event callback.
 	 */
 	private tryPatch(editor: Editor): void {
 		if (this.alreadyPatched) {
 			this.cancelPatchAttempt();
-			// eslint-disable-next-line no-unused-labels
 			DEVEL: console.warn('Animated cursor: try to patch the cursor while it has already been patched');
 			return;
 		}
 
-		// eslint-disable-next-line no-unused-labels
-		DEVEL: console.log('Animated Cursor: try to patch the cursor');
+		DEVEL: console.debug('Animated Cursor: try to patch the cursor');
 
 		let editorView = editor.cm,
-			cursorPlugin = hookCursorPlugin(editorView);
+			cursorPluginInstance = hookCursorPluginInstance(editorView);
 
-		if (!cursorPlugin?.value) {
-			// eslint-disable-next-line no-unused-labels
-			DEVEL: console.log('Animated Cursor: patch failed');
+		if (!cursorPluginInstance?.value) {
+			DEVEL: console.debug('Animated Cursor: patch failed');
 			return;
 		}
 
 		// Will be uninstalled automatically on plugin unload.
-		this.register(patchCursorLayer(cursorPlugin.value, this.settings));
+		patchCursorLayerConfig(this, cursorPluginInstance.value.layer);
 		this.alreadyPatched = true;
-		this.cursorPlugin = cursorPlugin;
+		this.cursorPlugin = cursorPluginInstance.spec;
 
-		// Detach the handler after a successful attemp.
+		// Post-patch action.
+		if (this.cursorPlugin) adjustCursorLayer(this.app, this.cursorPlugin);
+
+		// Unregister the handler after a successful attempt.
 		this.cancelPatchAttempt();
 
-		// eslint-disable-next-line no-unused-labels
-		DEVEL: console.log('Animated Cursor: patch successful');
+		DEVEL: console.debug('Animated Cursor: patch successful');
 	}
 
 	private cancelPatchAttempt(): void {
-		if (this.tryPatchRef) {
-			this.app.workspace.offref(this.tryPatchRef);
-			delete this.tryPatchRef;
+		if (this.patchAttemptRef) {
+			this.app.workspace.offref(this.patchAttemptRef);
+			delete this.patchAttemptRef;
 		}
 	}
 }
